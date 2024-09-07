af65e7d6eeb93c78c4f60976861c07aab623dab3
Felipe1501 <felipebsantos1501@gmail.com>
2024-09-06 21:19:24 -0300

    Commit TOTAL

---
+++ eleicoes/eleicao.py
@@ -0,0 +1,74 @@
+from datetime import date
+import pickle
+from typing import List
+from common import *
+
+class Urna:
+    mesario : Pessoa
+    __secao : int
+    __zona : int
+    __eleitores_presentes : List[Eleitor] = [0]
+    __votos = {} #dicionario chave = numero do candidato, valor é a quantidade de votos
+
+    def __init__(self, mesario : Pessoa, secao : int, zona : int,
+                 candidatos : List[Candidato], eleitores : List[Eleitor]):
+        self.mesario = mesario
+        self.__secao = secao
+        self.__zona = zona
+        self.__nome_arquivo = f'{self.__zona}_{self.__secao}.pkl'
+        self.__candidatos = candidatos
+        self.__eleitores = []
+        for eleitor in eleitores:
+            if eleitor.zona == zona and eleitor.secao == secao:
+                self.__eleitores.append(eleitor)
+
+        for candidato in self.__candidatos:
+            self.__votos[candidato.get_numero()] = 0
+        self.__votos['BRANCO'] = 0
+        self.__votos['NULO'] = 0
+
+        with open(self.__nome_arquivo, 'wb') as arquivo:
+            pickle.dump(self.__votos, arquivo)
+
+    def get_eleitor(self, titulo : int):
+        for eleitor in self.__eleitores:
+            if eleitor.get_titulo() == titulo:
+                return eleitor
+        return False
+
+    def registrar_voto(self, eleitor : Eleitor, n_cand : int):
+        self.__eleitores_presentes.append(eleitor)
+        if n_cand in self.__votos:
+            self.__votos[n_cand] += 1
+        elif n_cand ==0:
+            self.__votos['BRANCO'] += 1
+        else:
+            self.__votos['NULO'] += 1
+
+        with open(self.__nome_arquivo, 'wb') as arquivo:
+            pickle.dump(self.__votos, arquivo)
+    def emitirZeresima(self):
+
+
+
+        with open(f'zeresima_'+self.__nome_arquivo, 'wb') as arquivoZeresima:
+            pickle.dump(self.__votos, arquivoZeresima)
+        return self.__str__()
+
+
+
+
+
+
+
+    def __str__(self):
+        info = (f'Urna da seção {self.__secao}, zona {self.__zona}\n'
+                f'Mesario {self.mesario}\n')
+        data = date.today()
+        info += f'{data.ctime()}\n'
+        for k , v in self.__votos.items():
+            info+= f'Candidato {k} = {v} votos\n'
+
+        return info
+
+
